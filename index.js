a3af312ef1cd0b5fc857b0cd3049ac92e3714631
Jake Teton-Landis <just.1.jake@gmail.com>
2020-07-12 10:12:16 -0700

    allow passing emsdk sha

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const path = require('path');
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const tc = require('@actions/tool-cache');
+const path = require("path");
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const tc = require("@actions/tool-cache");
 
 async function getEmscripten(version) {
-  const emsdk_zip = await tc.downloadTool("https://github.com/emscripten-core/emsdk/archive/master.zip");
+  const ref = core.getInput("emsdk-version") || "master";
+  const emsdk_zip = await tc.downloadTool(
+    `https://github.com/emscripten-core/emsdk/archive/${ref}.zip`
+  );
   const extPath = await tc.extractZip(emsdk_zip);
-  const toolRoot = path.join(extPath, 'emsdk-master');
-  const emsdk_bin = path.join(toolRoot, 'emsdk');
+  const toolRoot = path.join(extPath, "emsdk-master");
+  const emsdk_bin = path.join(toolRoot, "emsdk");
 
   await exec.exec(emsdk_bin, ["install", version]);
   return await tc.cacheDir(toolRoot, "emscripten", version);
@@ -19,26 +22,26 @@ async function run(version) {
     console.log(`emscripten ${version} not found in the tool cache`);
     toolRoot = await getEmscripten(version);
   }
-  const emsdk_bin = path.join(toolRoot, 'emsdk');
+  const emsdk_bin = path.join(toolRoot, "emsdk");
 
   await exec.exec(emsdk_bin, ["activate", version]);
   let env_stdout = "";
-  await exec.exec(path.join(toolRoot, 'emsdk_env.sh'), [], {
+  await exec.exec(path.join(toolRoot, "emsdk_env.sh"), [], {
     listeners: {
-      stdout: data => env_stdout += data.toString()
-    }
+      stdout: (data) => (env_stdout += data.toString()),
+    },
   });
-  env_stdout.match(/(?<=(PATH \+= )).*/g).forEach(p => core.addPath(p))
-  env_stdout.match(/\w+ = .*/g).forEach(line => {
+  env_stdout.match(/(?<=(PATH \+= )).*/g).forEach((p) => core.addPath(p));
+  env_stdout.match(/\w+ = .*/g).forEach((line) => {
     let [varName, varVal] = line.split(" = ", 2);
     core.exportVariable(varName, varVal);
-  })
+  });
 }
 
 try {
-  const version = core.getInput('emscripten-version') || 'latest';
+  const version = core.getInput("emscripten-version") || "latest";
   console.log(`Installing emscripten (${version})...`);
-  run(version).catch(e => core.setFailed(e));
+  run(version).catch((e) => core.setFailed(e));
 } catch (error) {
   core.setFailed(error.message);
 }
